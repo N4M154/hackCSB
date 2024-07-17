4d68077b031ff953e6883034da345ebc3dcde109
Nabila Sheona <126275965+nabila-sheona@users.noreply.github.com>
2024-07-18 01:03:19 +0600

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -3,14 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import fileUpload from 'express-fileupload';
-import fs from 'fs';
-import { exec } from 'child_process';
+import fileUpload from "express-fileupload";
+import fs from "fs";
+import { exec } from "child_process";
 import path from "path";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-//import courseRoutes from "./routes/course.route.js";
 
 dotenv.config();
 
@@ -35,62 +34,49 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-//app.use(fileUpload());
-
-// Serve static files
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get('*', (req, res) => {
-const indexPath = path.resolve(__dirname, 'client', 'build', 'index.html');})
+app.use(fileUpload());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-//app.use("/api/course", courseRoutes);
 
-// Add the route for compiling C code
-// app.post('/api/compile', (req, res) => {
-//   if (!req.files || Object.keys(req.files).length === 0) {
-//     return res.status(400).send('No files were uploaded.');
-//   }
+app.post("/api/compile", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
-//   const file = req.files.file;
-//   const code = file.data.toString('utf8');
+  const file = req.files.file;
+  const code = file.data.toString("utf8");
 
-//   const uniqueId = uuidv4();
-//   const CODE_PATH = path.join(__dirname, `Code-${uniqueId}.c`);
-//   const EXE_PATH = path.join(__dirname, `Out-${uniqueId}.exe`);
+  const uniqueId = uuidv4();
+  const CODE_PATH = path.join(__dirname, `Code-${uniqueId}.c`);
+  const EXE_PATH = path.join(__dirname, `Out-${uniqueId}.exe`);
 
-//   // Save the uploaded code to a file
-//   fs.writeFileSync(CODE_PATH, code);
+  fs.writeFileSync(CODE_PATH, code);
 
-  // Compile the file
-//   exec(`gcc ${CODE_PATH} -o ${EXE_PATH}`, (compileError, compileStdout, compileStderr) => {
-//     if (compileError) {
-//       console.error('Compilation error:', compileStderr);
-//       return res.status(500).send({ output: `Compilation error: ${compileStderr}`, details: compileStderr });
-//     }
+  exec(`gcc ${CODE_PATH} -o ${EXE_PATH}`, (compileError, compileStdout, compileStderr) => {
+    if (compileError) {
+      console.error("Compilation error:", compileStderr);
+      return res.status(500).send({ output: `Compilation error: ${compileStderr}`, details: compileStderr });
+    }
 
-//     // Execute the compiled file
-//     exec(`${EXE_PATH}`, (execError, execStdout, execStderr) => {
-//       if (execError) {
-//         console.error('Execution error:', execStderr);
-//         return res.status(500).send({ output: `Execution error: ${execStderr}`, details: execStderr });
-//       }
+    exec(`${EXE_PATH}`, (execError, execStdout, execStderr) => {
+      if (execError) {
+        console.error("Execution error:", execStderr);
+        return res.status(500).send({ output: `Execution error: ${execStderr}`, details: execStderr });
+      }
 
-//       res.send({ output: execStdout });
+      res.send({ output: execStdout });
 
-//       // Clean up: Remove the created files
-//       fs.unlinkSync(CODE_PATH);
-//       fs.unlinkSync(EXE_PATH);
-//     });
-//   });
-// });
+      fs.unlinkSync(CODE_PATH);
+      fs.unlinkSync(EXE_PATH);
+    });
+  });
+});
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
-// Uncomment this section if you want to serve your React app from this server
-// 
-//   console.log('Trying to send:', indexPath);
-//   res.sendFile(indexPath);
-// });
